78f39e586ab6e5cfeaee9274c825530179b95eca
kevinpicon01 <digitalpruu@gmail.com>
2024-09-27 00:52:39 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,10 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Jugos Sofi";
+const email = "yennyleonm@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "+573157018821"; // Incluido el prefijo +57
+
 const textos = {
     index: {
         title: title,
@@ -12,25 +13,25 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "En Jugos Sofi, somos tu lugar favorito para disfrutar de un delicioso jugo de naranja, tinto y aromática de frutas. Nuestra misión es ofrecer dulzura y frescura al instante, brindándote bebidas que revitalizan y satisfacen.",
+        description2: "Cada jugo en Jugos Sofi está preparado con naranjas de la más alta calidad, seleccionadas para garantizar un sabor excepcional en cada sorbo. Nos esforzamos por ofrecer un servicio amigable y eficiente, haciendo que cada visita sea una experiencia agradable, asegurando que disfrutes al máximo de nuestras bebidas.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
         email: email
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Dulzura y frescura al instante.",
+            p2: ""
         },
-        address: "Dirección del emprendimiento",
+        address: "", // Información no proporcionada
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/profile.php?id=61565654442562",
+        instagram: "https://www.instagram.com", // Link base al no estar especificado
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
     },
-
 };
+
 export default textos;
